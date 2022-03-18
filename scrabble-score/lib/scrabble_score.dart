6f5a75589c35118177d08c3714ac89bb5379e8f7
gyu0710 <plame114@gmail.com>
2022-03-18 13:17:39 +0900

    edit

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,5 +1,5 @@
 int score(String? str) {
-  int cnt = 0;
+  int point = 0;
 
   List<String> value1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
   List<String> value2 = ["D", "G"];
@@ -14,21 +14,21 @@ int score(String? str) {
   } else {
     for (int i = 0; i < str.length; i++) {
       if (value1.contains(str.toUpperCase().split('')[i])) {
-        cnt += 1;
+        point += 1;
       } else if (value2.contains(str.toUpperCase().split('')[i])) {
-        cnt += 2;
+        point += 2;
       } else if (value3.contains(str.toUpperCase().split('')[i])) {
-        cnt += 3;
+        point += 3;
       } else if (value4.contains(str.toUpperCase().split('')[i])) {
-        cnt += 4;
+        point += 4;
       } else if (value5.contains(str.toUpperCase().split('')[i])) {
-        cnt += 5;
+        point += 5;
       } else if (value8.contains(str.toUpperCase().split('')[i])) {
-        cnt += 8;
+        point += 8;
       } else if (value10.contains(str.toUpperCase().split('')[i])) {
-        cnt += 10;
+        point += 10;
       }
     }
   }
-  return cnt;
+  return point;
 }
